7f4b3c780bc217741a5d8c883a72a7bd50538b59
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-24 01:50:28 +0600

    google sign in implemented

--- src/Pages/Shared/GoogleSignIn.js
+++ src/Pages/Shared/GoogleSignIn.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
+import Loading from './Loading';
 
 const GoogleSignIn = () => {
+    const { googleSignIn, loading, setLoading } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || '/';
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                navigate(from, { replace: true });
+            })
+            .catch(err => console.error(err))
+            .finally(setLoading(false))
+    }
+
     return (
         <div>
-            <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            <button onClick={handleGoogleSignIn} className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
         </div>
     );
 };
---
+++ src/contexts/AuthProvider.js
@@ -0,0 +1,73 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
+import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext();
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const logOut = () => {
+        setLoading(true);
+        localStorage.removeItem('carToken')
+        return signOut(auth);
+    };
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, provider)
+    }
+
+    const updateUser = (useInfo) => {
+        return updateProfile(auth.currentUser, useInfo);
+    }
+
+    const resetPassword = email => {
+        setLoading(true)
+        return sendPasswordResetEmail(auth, email)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, [])
+
+
+    const authInfo = {
+        createUser,
+        signIn,
+        user,
+        logOut,
+        updateUser,
+        loading,
+        googleSignIn,
+        setLoading,
+        resetPassword
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
