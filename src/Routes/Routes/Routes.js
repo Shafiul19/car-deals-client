c6dbc2aa210cbbe409e55b7ff7020162193c7d4b
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-24 20:46:54 +0600

    useAdmin hook and private route created

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,9 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../../layouts/DashboardLayout";
 import Main from "../../layouts/Main";
 import Blogs from "../../Pages/Blogs/Blogs";
+import AllBuyers from "../../Pages/Dashboard/AllBuyers/AllBuyers";
+import AllSellers from "../../Pages/Dashboard/AllSellers/AllSellers";
+import MyOrders from "../../Pages/Dashboard/MyOrders/MyOrders";
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Signup from "../../Pages/Signup/Signup";
+import AdminRoutes from "../AdminRoute/AdminRoute";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 
@@ -36,8 +40,17 @@ const router = createBrowserRouter([
         element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
         children: [
             {
-
-            }
+                path: '/dashboard/myorders',
+                children: <MyOrders></MyOrders>
+            },
+            {
+                path: '/dashboard/allbuyers',
+                children: <AdminRoutes><AllBuyers></AllBuyers></AdminRoutes>
+            },
+            {
+                path: '/dashboard/allsellers',
+                children: <AdminRoutes><AllSellers></AllSellers></AdminRoutes>
+            },
         ]
     }
 ])
